d8f45fab05e5d8255a0d5b5a9b672212a307e34d
Jacob Fadich <jacobgamer137@yahoo.com>
2022-04-14 21:49:50 -0700

    fix?

--- index.js
+++ index.js
@@ -6,17 +6,17 @@ Do you follow , or stay?`)
 if(secondAnswer === "follow"){ 
     let thirdAnswer = window.prompt("You follow the cat to a colony of cats, nestled in a fort of warm blankets and subsisting off of inexplicably warm soup. They are content with you staying, but you wonder if you should alert the world to this magical safe haven.") 
 } 
-    else if(thirdAnswer === "stay"){ 
+    else if(secondAnswer === "stay"){ 
 let thirdAnswer = window.prompt("You live happily amongst the cats.")}
 
 } else if(firstAnswer === 'right') {
     let secondAnswer = window.prompt(`You come across a snoring dragon. 
 On the other side of him, you see a shiny chest of treasure. Another path would 
 lead you away from the dragon altogether. Which path do you take, chest or leave?`)
-if(thirdAnswer === "chest"){
+if(secondAnswer === "chest"){
     let thirdAnswer = ("The dragon wakes up and eats you.")
 }
-else if(thirdAnswer === "leave"){
+else if(secondAnswer === "leave"){
     let thirdAnswer = window.prompt("You come across a sword in a lake, upon pikcing it up you awake as if from a dream ruling your own kingdom.")}
 
 
