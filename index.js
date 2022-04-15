a11d058df36fb37780236d0417ddfbb1fa33f513
Jacob Fadich <jacobgamer137@yahoo.com>
2022-04-14 21:46:15 -0700

    eh

--- index.js
+++ index.js
@@ -7,8 +7,8 @@ if(secondAnswer === "follow"){
     let thirdAnswer = window.prompt("You follow the cat to a colony of cats, nestled in a fort of warm blankets and subsisting off of inexplicably warm soup. They are content with you staying, but you wonder if you should alert the world to this magical safe haven.") 
 } 
     else if(thirdAnswer === "stay"){ 
-let thirdAnswer = window.prompt("You live happily amongst the cats.")
-}
+let thirdAnswer = window.prompt("You live happily amongst the cats.")}
+
 } else if(firstAnswer === 'right') {
     let secondAnswer = window.prompt(`You come across a snoring dragon. 
 On the other side of him, you see a shiny chest of treasure. Another path would 
@@ -17,7 +17,7 @@ if(thirdAnswer === "chest"){
     let thirdAnswer = ("The dragon wakes up and eats you.")
 }
 else if(thirdAnswer === "leave"){
-    let thirdAnswer = window.prompt("You come across a sword in a lake, upon pikcing it up you awake as if from a dream ruling your own kingdom.")
-}
+    let thirdAnswer = window.prompt("You come across a sword in a lake, upon pikcing it up you awake as if from a dream ruling your own kingdom.")}
+
 
 }
